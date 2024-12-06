bebca2370cf25191e8f9794aae05a9d7d39523f3
Chiru1985 <chiranjeevi.vattiri@hyperisland.se>
2024-12-06 10:42:47 +0100

    Updated the Checkout

--- src/partials.js
+++ src/partials.js
@@ -1,7 +1,9 @@
 import header from "./template/_partial/header.ejs"
 import footer from "./template/_partial/footer.ejs"
+import checkout from "./template/_partial/checkout.ejs"
 
 export default {
     header,
-    footer
+    footer,
+    checkout
 }
