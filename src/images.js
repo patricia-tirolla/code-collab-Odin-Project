eafe8e787a1e9002366c8c53954a4d4dfd93ea35
NaghmehGhorashi <Naghmeh.Ghorashi@hyperisland.se>
2024-12-06 13:34:24 +0100

    add navbar (#4)
    Co-authored-by: Naghmeh Ghorashi <naghmeh.ghorashi@gmail.com>
    Co-authored-by: Patricia Tirolla <179636704+patricia-tirolla@users.noreply.github.com>

--- src/images.js
+++ src/images.js
@@ -1,5 +1,24 @@
 import creditCard from "./img/credit-card.png"
+import facebook from "./img/facebook.png"
+import linkedin from "./img/linkedin.png"
+import shieldKeyhole from "./img/shield-keyhole.png"
+import shippingFast from "./img/shipping-fast.png"
+import shoppingCart from "./img/shopping-cart.png"
+import telegram from "./img/telegram.png"
+import twitter from "./img/twitter.png"
+import youtube from "./img/youtube.png"
+import shoppingcartadd from "./img/shoppingcartadd.png"
 
 export default {
     creditCard,
+    facebook,
+    linkedin,
+    shieldKeyhole,
+    shippingFast,
+    shoppingCart,
+    telegram,
+    twitter,
+    youtube, 
+    shoppingcartadd
+
 }
--- src/partials.js
+++ src/partials.js
@@ -1,8 +1,10 @@
 import header from "./template/_partial/header.ejs"
 import footer from "./template/_partial/footer.ejs"
+import navbar from "./template/_partial/navbar.ejs"
 import checkout from "./template/_partial/checkout.ejs"
 
 export default {
+    navbar,
     header,
     footer,
     checkout
