2d57ff714bc35e0f3ac41cae95b9f8070944e855
Patricia Tirolla <179636704+patricia-tirolla@users.noreply.github.com>
2024-12-09 10:46:53 +0100

    Shopping cart patricia (#5)
    * Creates shopping cart list
    
    The function to add a product now adds it to the
    shopping cart list
    
    * Add local storage to shopping cart
    
    * Shopping cart functions refactored
    
    * Add functions to "remove" buttons
    
    Now it's possible to remove items from the
    shopping cart.

--- src/page/shoppingCartPage.js
+++ src/page/shoppingCartPage.js
@@ -1,3 +1,76 @@
-export function addProductToShoppingCart() {
-    console.log("it's added to cart!")
-}
+import cartTemplate from "../template/cart.ejs";
+import partials from "../partials";
+
+function getCartFromLocalStorage() {
+    const cart = localStorage.getItem("shoppingCart");
+    if (cart === null) {
+        return [];
+    }
+    return JSON.parse(cart);
+};
+
+export function addProductToShoppingCart(product) {
+    let shoppingCart = getCartFromLocalStorage();
+
+    shoppingCart.push(product);
+    localStorage.setItem("shoppingCart", JSON.stringify(shoppingCart));
+};
+
+function removeProductFromShoppingCart(index) {
+    let shoppingCart = getCartFromLocalStorage();
+
+    shoppingCart.splice(index, 1);
+    localStorage.setItem("shoppingCart", JSON.stringify(shoppingCart));
+};
+
+function removeItemFromCartButton() {
+    const removeButtonList = document.querySelectorAll(".remove-item-btn");
+    removeButtonList.forEach(button => {
+        button.addEventListener("click", () => {
+            removeProductFromShoppingCart(button.dataset.index);
+            loadCartPage();
+        })
+    });
+};
+
+function sumCartItems() {
+    return {
+        getPriceList(list) {
+            let itemsPrice = [];
+            for (let i = 0; i < list.length; i++) {
+                itemsPrice.push(list[i].price);
+            }
+            return itemsPrice;
+        },
+        sumProductsInTheCart(priceList) {
+            let sum = 0;
+            for (let i = 0; i < priceList.length; i++){
+                sum += priceList[i];
+            }
+            return sum;
+        },
+        priceList() {
+            return this.getPriceList(getCartFromLocalStorage());
+        },
+        subtotal() {
+            return this.sumProductsInTheCart(this.priceList());
+        },
+        discount() {
+            return 0;
+        },
+        total() {
+            return (this.subtotal() + this.discount());
+        }
+    }
+};
+
+const cartTotals = sumCartItems();
+
+export function loadCartPage() {
+    document.querySelector("body").innerHTML = cartTemplate({
+        partials, cart: getCartFromLocalStorage(), 
+        subtotal: cartTotals.subtotal(), 
+        total: cartTotals.total()});
+
+    removeItemFromCartButton();
+};
