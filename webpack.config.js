1fc8b5ed6c9452dbef43c5dd88eff828d92e0e80
Patricia Tirolla <patricia.tirolla@hyperisland.se>
2024-12-09 21:13:45 +0100

    Fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -32,11 +32,6 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.jsx?$/,
-        exclude: ['node_modules'],
-        use: ['babel-loader'],
-      },
-      {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
