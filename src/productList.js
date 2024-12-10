fa5f1b90be62f3bef6cbba3cec072af1bb6929ee
Patricia Tirolla <patricia.tirolla@hyperisland.se>
2024-12-10 14:51:28 +0100

    Fix footer copyright and final css

--- src/productList.js
+++ src/productList.js
@@ -27,39 +27,17 @@ export const productList = (containerId, templateId) => {
         throw new Error("Failed to fetch data");
       }
       const data = await response.json();
-      console.log("json data:", data);
       return data;
     } catch (error) {
       console.error("Error fetching data:", error.message);
     }
   };
 
-  // const formatPrice = (num) => {
-  //   return num.toFixed(2);
-  // };
-
-  // const formatLength = (str) => {
-  //   if (str.length <= 100) {
-  //     return str;
-  //   } else {
-  //     return str.slice(0, 100) + "...";
-  //   }
-  // };
-
-  // const filterData = (data, category) => {
-  //   if (category === "All") {
-  //     return data;
-  //   } else {
-  //     return data.filter((item) => item.category === category);
-  //   }
-  // };
-
   const displayData = async (url, category) => {
     const productSection = document.getElementById(containerId);
     productSection.innerHTML = "";
 
     const data = await fetchData(url);
-    console.log("data from fetch:", data);
 
     const filteredData = filterData(data, category);
     //render the product card
@@ -94,22 +72,17 @@ export const productList = (containerId, templateId) => {
     const buttonContainer = document.getElementById("button-container");
 
     const data = await fetchData(url);
-    console.log("data from fetch:", data);
 
     const categories = data.map((item) => item.category);
-    console.log("category:", categories);
 
     const uniqueCategories = ["All", ...new Set(categories)];
-    console.log("unique categories:", uniqueCategories);
 
     // render the category buttons
     uniqueCategories.forEach((category) => {
       const categoryButton = document.createElement("button");
       categoryButton.classList.toggle("category-button");
       const uppercase = category.slice(0, 1).toUpperCase();
-      console.log(uppercase);
       const newWord = uppercase + category.slice(1);
-      console.log(newWord);
       categoryButton.setAttribute("data-category", category);
 
       categoryButton.textContent = newWord;
@@ -118,7 +91,6 @@ export const productList = (containerId, templateId) => {
       categoryButton.addEventListener("click", (e) => {
         displayData(url, category);
         const buttonCategory = e.target.textContent;
-        console.log("button click category:", buttonCategory);
 
         const h1 = document.getElementById("category-h1");
         h1.textContent = `${buttonCategory}`;
